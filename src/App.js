b4985a8b0e01ad085d2d092efa96169be3d6ce2a
Aléxis Abner Cordeiro da Silva <121447713+AlexisCordeiro@users.noreply.github.com>
2024-03-06 19:49:19 -0300

    atual

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Calc from './components/Calc/calc';
 const App = () => {
   return (
     <div>
-      <h1>Calculadora do Aléxis</h1>
+      <h1>Calculadora do Aléxis, Bruno e Lucas</h1>
       <Calc />
     </div>
   );
